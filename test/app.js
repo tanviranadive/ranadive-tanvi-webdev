a23bc9dfd0ef1d92e305f3ce25effedf6c2b4c41
tanvi ranadive <tanvi@ccs.neu.edu>
2017-04-15 04:58:40 -0400

    Updated test app

--- test/app.js
+++ test/app.js
@@ -10,13 +10,6 @@ module.exports = function(app)
         connectionString = process.env.MONGODB_URI
     }
 
-    /*if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
-    }*/
 
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
