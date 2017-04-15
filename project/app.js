8493e1850eef30e9a6e0a7c7c2399a2140e1b9a1
tanvi ranadive <tanvi@ccs.neu.edu>
2017-04-15 13:46:34 -0400

    updated connection mlab string

--- project/app.js
+++ project/app.js
@@ -5,6 +5,26 @@
 
 module.exports = function(app) {
 
+    var connectionString = 'mongodb://127.0.0.1:27017/project';
+
+    /*if(process.env.MONGODB_URI){
+     connectionString = process.env.MONGODB_URI
+     }*/
+
+    if(process.env.MLAB_USERNAME){
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+
+
+
+    var mongoose = require("mongoose");
+    mongoose.connect(connectionString);
+
+
     var models = require('./model/models.server')();
     //var websiteModel = require('./model/website/website.model.server')();
     //console.log("in app ");
