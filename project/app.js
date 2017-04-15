eed46a18453302a3a434de98a6ee3eb6ab6778f1
tanvi ranadive <tanvi@ccs.neu.edu>
2017-04-15 13:48:20 -0400

    Updated to mongodb uri

--- project/app.js
+++ project/app.js
@@ -7,17 +7,17 @@ module.exports = function(app) {
 
     var connectionString = 'mongodb://127.0.0.1:27017/project';
 
-    /*if(process.env.MONGODB_URI){
+    if(process.env.MONGODB_URI){
      connectionString = process.env.MONGODB_URI
-     }*/
+     }
 
-    if(process.env.MLAB_USERNAME){
+    /*if(process.env.MLAB_USERNAME){
         connectionString = process.env.MLAB_USERNAME + ":" +
             process.env.MLAB_PASSWORD + "@" +
             process.env.MLAB_HOST + ':' +
             process.env.MLAB_PORT + '/' +
             process.env.MLAB_APP_NAME;
-    }
+    }*/
 
 
 
